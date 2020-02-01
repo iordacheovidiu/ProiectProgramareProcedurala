11c46e4a34ebfae32bbefe72b90e36952cef69af
Ovidiu <Ovidiu@DESKTOP-7J47U7V>
2020-02-01 16:12:07 +0200

    Tema - Ex.2

---
+++ src/main/Subsitem.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Subsitem {
+
+	public static void main(String[] args) {
+		double d = 2.95;
+		int i = 4;
+		System.out.println(++d>i?d:i);
+		
+		/* este instructiune if-else scrisa in linie valoare expresiei este 4 deoarece il incrementam pe "d" cu 1 iar noua 
+		 valoare este 3.95, acesta nu respecta conditia ca "++d>i=false" si ii este atribuita valoarea lui "i"  */
+}
+
+}
