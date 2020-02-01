40afcc90961161e5d13af3ec399b7ff5fe56a4b0
Ovidiu <Ovidiu@DESKTOP-7J47U7V>
2020-02-01 17:52:02 +0200

    Tema 5

---
+++ src/main/Tema_5.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Tema_5 {
+	public static void main(String[] args){
+		int i = 1;
+		int fact = 1;
+		int n = 13;		
+		
+			for(i = 1; i < n; i++) {
+				
+				fact = fact * n;
+			}				
+System.out.println("n!= " + fact);	
+			
+}
+
+}
