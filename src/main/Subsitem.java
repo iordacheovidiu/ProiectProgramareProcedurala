89aaaf92a9a10b803a23a9c860b763e3ff77b5eb
Ovidiu <Ovidiu@DESKTOP-7J47U7V>
2020-02-01 16:15:29 +0200

    Tema - Ex.2

--- src/main/Subsitem.java
+++ src/main/Subsitem.java
@@ -8,7 +8,7 @@ public class Subsitem {
 		System.out.println(++d>i?d:i);
 		
 		/* este instructiune if-else scrisa in linie valoare expresiei este 4 deoarece il incrementam pe "d" cu 1 iar noua 
-		 valoare este 3.95, acesta nu respecta conditia ca "++d>i=false" si ii este atribuita valoarea lui "i"  */
+		 valoare este 3.95, acesta nu respecta conditia ca "++d>i=true" si ii este atribuita valoarea lui "i"  */
 }
 
 }
