fbec4c2164ce1045e1b14f627a755f8094f3fd42
Ovidiu <Ovidiu@DESKTOP-7J47U7V>
2020-02-02 16:14:44 +0200

    Tema_8

---
+++ src/main/Tema_8.java
@@ -0,0 +1,42 @@
+package main;
+
+public class Tema_8 {
+	
+	public static void main (String args[] ) {
+		
+		int ziua = 7;
+		
+		switch (ziua) {
+		
+		case 1:
+			System.out.print("Luni");
+			break;
+			
+		case 2:
+			System.out.print("Marti");
+			break;
+			
+		case 3:
+			System.out.print("Miercuri");
+			break;
+			
+		case 4:
+			System.out.print("Joi");
+			break;
+			
+		case 5:
+			System.out.print("Vineri");
+			break;
+			
+		case 6:
+			System.out.print("Sambata");
+			break;
+			
+		case 7:
+			System.out.print("Duminica");
+			break;
+		}
+		
+	}
+
+}
