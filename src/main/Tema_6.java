51371ceb7b00b1766d01aaa2cf8a5ef2aa9b2097
Ovidiu <Ovidiu@DESKTOP-7J47U7V>
2020-02-02 15:38:42 +0200

    Tema_6

---
+++ src/main/Tema_6.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Tema_6 {
+
+	public static void main(String[] args){
+		int a = 10; 
+		int	b = 7; 
+		int	c = 130; 
+		
+		int min = 7;
+		
+	if(a<=min) {
+		System.out.print("cel mai mic numar este " +min);
+	}else {
+		System.out.print("");
+	}
+	if(b<=min) {
+		System.out.print("cel mai mic numar este " + min);
+	}else {
+		System.out.print("");
+	}
+	if(c<=min)	{
+		System.out.print("cel ami mic numar este " +min);
+	}else {
+		System.out.print("");
+	}
+	}	
+	
+}
