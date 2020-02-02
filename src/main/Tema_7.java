c72a99b9c8965a05ae80586d6eda8e82e53cc4e9
Ovidiu <Ovidiu@DESKTOP-7J47U7V>
2020-02-02 16:05:04 +0200

    Tema_7

---
+++ src/main/Tema_7.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Tema_7 {
+	
+	public static void main (String args[]) {
+		int numar = 3;
+		if (numar % 2 == 0) {
+		    System.out.println("numarul este par");
+		   } else {
+		    System.out.println("numarul est impar");
+		  }
+		 }
+
+}
